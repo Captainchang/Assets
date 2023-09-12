a69448b8c3cce13635caf9d77783c4ef397baf26
DESKTOP-65UJOBC\ckdan <ckdanr14@naver.com>
2023-09-12 15:29:02 +0900

    09/12

--- UI/ItemDragHandler.cs
+++ UI/ItemDragHandler.cs
@@ -12,7 +12,8 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler,IDragHandler , I
     [SerializeField]
     private Vector3 originPosition;
 
-    public int slotIndex; // 슬롯 인덱스
+    [SerializeField]
+    private Inventory currentSlot;
 
     private void Start()
     {
@@ -24,8 +25,8 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler,IDragHandler , I
     
     public void OnBeginDrag(PointerEventData eventData)
     {
-       // canvasGroup.blocksRaycasts = false;
-       // transform.SetParent(transform.parent.parent); // 위치를 바꿀 준비를 위해  부모를 바꿈
+        transform.SetParent(GameObject.FindGameObjectWithTag("UI Canvas").transform); // 위치를 바꿀 준비를 위해  부모를 바꿈
+        // To do  포지션을 마우스커서로.
         transform.localPosition = originPosition;
     }
     public void OnDrag(PointerEventData eventData)
@@ -34,23 +35,8 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler,IDragHandler , I
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        //canvasGroup.blocksRaycasts = true;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
-        
-
-        //  To do  이거말고 슬롯 스왑을 만들자.
-        if (hit.collider != null && hit.collider.CompareTag("Slot"))
-        {
-            transform.SetParent(hit.collider.transform);
-            transform.localPosition = Vector3.zero;
-            slotIndex = hit.collider.GetComponent<ItemDragHandler>().slotIndex;
-        }
-        else
-        {
-            //원래 위치로
-            transform.SetParent(originParent);
-            transform.localPosition = originPosition;
-        }
+          transform.SetParent(originParent);
+          transform.localPosition = originPosition;
+          Debug.Log("슬롯이 아닌곳");
     }
 }
